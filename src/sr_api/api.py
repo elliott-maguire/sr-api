6fbbc6679eddcfe6b5537233e8c58b7f44b9aeca
Elliott Maguire <etgmag@comcast.net>
2018-11-22 14:20:35 -0800

    initial commit

---
+++ src/sr_api/api.py
@@ -0,0 +1,21 @@
+""" Initialize SmartRecruiters API client. """
+
+# utility module imports go here
+
+
+class SmartAPI:
+    def __init__(self, token):
+        """ Load all API classes, sets up instance.
+        
+        Arguments:
+            str:token -- a certified SmartToken
+
+        """
+    
+    def get_instance(self):
+        """ Get platform instance. """
+
+    def setup_database(self):
+        """ Set up local ERGAL database. """
+
+        
